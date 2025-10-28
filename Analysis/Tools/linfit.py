bfac1bc1bf81709de8f26ffb24f019bd43e375ac
Aryaman Gupta <mert4366@ox.ac.uk>
2025-10-28 15:42:00 +0000

    Fixed linfit test

--- Analysis/Tools/linfit.py
+++ Analysis/Tools/linfit.py
@@ -2,11 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from pathlib import Path
-from load_data import vel_data
-import zipfile
+from Analysis.load_data import vel_data
+from decimal import Decimal
 
-def model(df= vel_data(), station_name="NLBS", DOY=6):
-    df = vel_data()
+
+def model(df=vel_data(), station_name="NLBS", DOY=6):
+    
     print(f"station selected for linear fit is {station_name}")
 
     doy_col = f"{station_name}_DOY"
@@ -17,12 +18,11 @@ def model(df= vel_data(), station_name="NLBS", DOY=6):
     mdl_frame = pd.DataFrame({"DOY": x, "Vel": y})
     mdl_select = mdl_frame[mdl_frame["DOY"].apply(int) == DOY]
 
-
     model = LinearRegression()
     model.fit(mdl_select[["DOY"]], mdl_select["Vel"])
 
     coeff = float(model.coef_)
-    inter = model.intercept_
+    inter = float(model.intercept_)
 
     return(coeff, inter)
 
--- Tests/test_linfit.py
+++ Tests/test_linfit.py
@@ -2,16 +2,18 @@ import numpy.testing as npt
 import numpy as np
 import pandas as pd
 from Analysis.Tools.linfit import model
+import math
+import inspect
 
 def test_find_slope():
-    x = y = [0, 1]
-    test_df=pd.DataFrame({"x": x, "y": y})
-    coeff, intercept = model(df=test_df)
-    assert(coeff == 1)
+    x = y = [0.1, 0,2]
+    test_df=pd.DataFrame({"NLBS_DOY": x, "NLBS_VEL": y})
+    coeff, intercept = model(df=test_df, DOY=0)
+    assert math.isclose(coeff, 1.0)
 
 def test_find_intercept():
-    x = [0, 1]
-    y = [-1,0]
-    test_df=pd.DataFrame({"x": x, "y": y})
-    coeff, intercept = model(df=test_df)
-    assert(intercept == -1)
+    x = [-0.1, 0]
+    y = [0, 0.1]
+    test_df=pd.DataFrame({"NLBS_DOY": x, "NLBS_VEL": y})
+    coeff, intercept = model(df=test_df, DOY=0)
+    assert math.isclose(intercept, 0.1)
