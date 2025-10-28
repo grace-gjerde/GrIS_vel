a05edb0c60db5422c34ab88ddc5730e9162682ef
Aryaman Gupta <mert4366@ox.ac.uk>
2025-10-28 10:50:32 +0000

    load_data

---
+++ Analysis/load_data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from pathlib import Path
+import zipfile
+
+zip_path = Path("../Data/Glacier.zip")  # your .zip file
+extract_dir = Path("../Data/")          # folder to extract to
+
+def extract():
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        zip_ref.extractall(extract_dir)
+    print("Extraction complete")
+
+def stations():
+    df = pd.read_csv("../Data/Glacier/Glacier_transient_vel.csv")
+#Identify stations and their velocity data
+stations = []
+for col in df.columns:
+    if "_DOY" in col:
+        station = col.split("_DOY")[0]
+        vel_col = f"{station}_VEL"
+        if vel_col in df.columns:
+            stations.append(station)
