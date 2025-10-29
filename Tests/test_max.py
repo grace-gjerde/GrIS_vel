46fadcb49b1725deb3366992fb37ce9aa4464d8e
Grace Gjerde <grace.gjerde@reuben.ox.ac.uk>
2025-10-29 13:08:11 +0000

    edited test_max.py test data arrays

--- Tests/test_max.py
+++ Tests/test_max.py
@@ -1,6 +1,5 @@
 # test_max.py
 
-import numpy as np
 from Analysis.Tools.find_max import max_vel
 from Analysis.load_data import vel_data, read_stations, extract
 
@@ -9,15 +8,15 @@ df=vel_data()
 stations=read_stations()
 
 def test_max_integers():
-    test_input = np.array([1, 2, 3, 4])
+    test_input = [1, 2, 3, 4]
     assert max_vel(test_input) == 4
 
 def test_max_zeros():
-    test_input = np.array([0, 0, 0, 0])
+    test_input = [0, 0, 0, 0]
     assert max_vel(test_input) == 0
 
 def test_max_negatives():
-    test_input = np.array([-2, -1, 0, 1])
+    test_input = [-2, -1, 0, 1]
     assert max_vel(test_input) == 1
 
 def test_station_max_values():
