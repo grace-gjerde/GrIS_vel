e9faaafcbf7beeced8b4285406ab32b0f7cd84d1
Rebecca Tichford <rebecca.tichford@btinternet.com>
2025-10-30 14:24:34 +0000

    Added comments explaining code to tests files

--- Tests/test_linfit.py
+++ Tests/test_linfit.py
@@ -1,3 +1,6 @@
+#Test_linfit.py
+
+#Import modules
 import numpy.testing as npt
 import numpy as np
 import pandas as pd
@@ -5,14 +8,17 @@ from Analysis.Tools.linfit import model
 from Analysis.load_data import extract
 import math
 
+#Unzip data file
 extract()
 
+#Check the gradient function is correct for a set of test data
 def test_find_slope():
     x = y = [0.1, 0,2]
     test_df=pd.DataFrame({"NLBS_DOY": x, "NLBS_VEL": y})
     coeff, intercept = model(df=test_df, DOY=0)
     assert math.isclose(coeff, 1.0)
 
+#Check the y-intercept function is correct for a set of test data
 def test_find_intercept():
     x = [-0.1, 0]
     y = [0, 0.1]
